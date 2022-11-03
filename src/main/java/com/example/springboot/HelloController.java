d1acebfefb43e041e0cf50f71a92daa2903f368c
guy-ubuntu-desktop <guy@wizards.co.il>
2022-11-03 17:15:00 +0200

    first commit

---
+++ src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		String x="aa";
+
+		String a="David";
+
+
+
+
+
+
+
+
+		return a;
+	}
+
+}
